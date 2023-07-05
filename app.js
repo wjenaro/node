b30f624c34cbddbabc69b200f4f815837a670a43
Jenaro Wachira <wjenaro@gmail.com>
2023-07-05 17:19:54 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const amount=12
+if (amount <10){
+    console.log('small number');
+}
+else{
+    console.log("Hello world, this is my first node js app \n it is a  big number");
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+//const hello= require("fs");
+//hello.writeFile("message.txt", "Hello from NodeJS!", (err)=> {
+  //  if(err) throw err;
+   // console.log("The file has been saved!");
+//});
+const os=require('os');
+console.log(os.version());
