8fe53c2d547b91f7715bd2d8dec978c715d1cb99
Jenaro Wachira <wjenaro@gmail.com>
2023-07-31 23:50:11 +0300

    7/31

--- 4.4 Band Generator Project/index.js
+++ 4.4 Band Generator Project/index.js
@@ -1,11 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
-
 const app = express();
 const port = 3000;
 
 //Step 3 - Make the styling show up.
 //Hint 1: CSS files are static files!
+app.use(express.static('public'));
 //Hint 2: The header and footer are partials.
 //Hint 3: Add the CSS link in header.ejs
 
@@ -15,7 +15,8 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  //Step 1 - Make the get route work and render the index.ejs file.
+  res.render('index.ejs');
+  
 });
 
 app.post("/submit", (req, res) => {
